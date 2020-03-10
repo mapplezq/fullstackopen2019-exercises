900f314e6beea10f28419b4040ce9b7941582a5b
Mapple <mapplezq@gmail.com>
2020-03-10 20:39:12 +0800

    add countries step2

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -18,13 +18,41 @@ const SingleCountry = (props) => {
   )
 }
 
+const CountryListItem = (props) => {
+  const [showDetail, setShowDetail ] = useState(false)
+
+  const showDetailBtnClick = (event) => {
+    const clicked = !showDetail
+    setShowDetail(clicked)
+  }
+  if(!showDetail) {
+    return (
+      <div>
+          <label >{props.country.name}</label>
+          <button onClick={showDetailBtnClick}>{showDetail?'hide': 'show'}</button>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+          <label >{props.country.name}</label>
+          <button onClick={showDetailBtnClick}>{showDetail?'hide': 'show'}</button>
+          <SingleCountry country={props.country}/>
+      </div>
+    )
+  }
+  
+}
+
 const Show = (props) => {
   if (props.countries.length > 10) {
     return (
       <p>Too many matches, specify another filter</p>
     )
  } else if (props.countries.length > 1) {
-   return props.countries.map(country => <p key={country.name}>{country.name}</p>)
+   return props.countries.map(country => 
+      <CountryListItem  key={country.name} country={country} />
+    )
  } else if (props.countries.length === 1) {
    return <SingleCountry country={props.countries[0]}/>
  } else {
